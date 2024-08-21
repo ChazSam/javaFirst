1b30527ba60373c8dc2d25aeeb241048568181d4
Charles <Charlessammarco@gmail.com>
2024-08-20 21:30:15 -0500

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        
+        String name = scanner.nextLine();   
+
+        System.out.println("Hello World " + name + "!");
+
+        scanner.close();
+    }
+}
